9f87a7e79b85c60a0647c55da4e374a06cabf4d8
allantabilog <allan.tabilog@gmail.com>
2024-11-03 21:21:59 +1100

    Code improvements

--- go-server/main.go
+++ go-server/main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
-var response map[string]string = make(map[string]string)
+var (
+	logger = log.New(os.Stdout, "logger: ", log.Lshortfile)
+)
 
 func main() {
 	// setup a simple http server
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Content-Type", "application/json")
+	const PORT = 8080
+	http.HandleFunc("/", handlePingRequest);
+	logger.Printf("Server is running on port %v\n", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+}
 
-		// add a message to the response map
-		response["message"] = "json-server is up"
-		response["timestamp"] = time.Now().Format(time.RFC3339)
+func handlePingRequest(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("Request received from %v\n", r.RemoteAddr)
+	var response map[string]string = make(map[string]string)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(jsonResponse)
-	})
-	http.ListenAndServe(":8080", nil)
+	// add a message to the response map
+	response["message"] = "fibonacci-server is up"
+	response["timestamp"] = time.Now().Format(time.RFC3339)
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	logger.Printf("Returning response: %v\n", string(jsonResponse))
+	w.Write(jsonResponse)
 }
--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-  const [serverTimestamp, setServerTimestamp] = useState("");
+  // initialise a piece of state that will hold
+  // json data
+  const [message, setMessage] = useState("<Loading message...>");
+  const [serverTimestamp, setServerTimestamp] = useState(
+    "<Loading timestamp...>"
+  );
   useEffect(() => {
-    fetch("http://localhost:8080/")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.log(error));
-  });
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/");
+        const data = await response.json();
+        setMessage(data.message);
+        setServerTimestamp(data.timestamp);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Setting up the visualisation service (watch this space)</p>
-        <p>Response from server: {message}</p>
+        <p>
+          Response from server:{" "}
+          <span style={{ color: "yellow" }}>{message}</span> on{" "}
+          <span style={{ color: "yellow" }}>
+            {new Date(serverTimestamp).toLocaleString()}
+          </span>
+        </p>
       </header>
     </div>
   );
