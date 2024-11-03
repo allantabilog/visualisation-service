e0053ab920770147bfd49e4b8dd387c6f280e404
allantabilog <allan.tabilog@gmail.com>
2024-11-03 21:40:57 +1100

    Add handler for fibonacci

--- go-server/main.go
+++ go-server/main.go
@@ -4,18 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 var (
 	logger = log.New(os.Stdout, "logger: ", log.Lshortfile)
+	fibs = make(map[int]int)
 )
 
 func main() {
 	// setup a simple http server
 	const PORT = 8080
+	http.HandleFunc("/fibonacci", handleFibonacciRequest);
 	http.HandleFunc("/", handlePingRequest);
 	logger.Printf("Server is running on port %v\n", PORT)
 	http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
@@ -38,4 +42,51 @@ func handlePingRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	logger.Printf("Returning response: %v\n", string(jsonResponse))
 	w.Write(jsonResponse)
+}
+
+func handleFibonacciRequest(w http.ResponseWriter, r *http.Request) {
+	// handle fibonacci requests
+	logger.Printf("Request received from %v\n", r.RemoteAddr)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	query := r.URL.Query()
+	nStr := query.Get("n")
+	if nStr == "" {
+		http.Error(w, "Query parameter 'n' is required", http.StatusBadRequest)
+		return
+	}
+
+	n, err := strconv.Atoi(nStr)
+	if err != nil {
+		http.Error(w, "Query parameter 'n' must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	FibonacciMemoised(n)
+	jsonResponse, err := json.Marshal(fibs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Returning response: %v\n", string(jsonResponse))
+	w.Write(jsonResponse)
+}
+
+func FibonacciMemoised(n int) int {
+	logger.Printf("Calculating Fibonacci number for %v\n", n)
+	// check if the Fibonacci number is already memoised
+	if val, ok := fibs[n]; ok {
+		return val
+	}
+	// calculate the Fibonacci number
+	if n <= 1 {
+		fibs[n] = n
+	} else {
+		fibs[n] = FibonacciMemoised(n-1) + FibonacciMemoised(n-2)
+	}
+	if fibs[n] > math.MaxInt64 - 1 {
+		panic("Fibonacci number is too large")
+	}
+	logger.Printf("Fibonacci number for %v is %v\n", n, fibs[n])
+	return fibs[n]
 }
